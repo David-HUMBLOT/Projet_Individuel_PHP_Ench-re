d0ebb546f5fa785f130b9fdb985a9242b65626fe
Dada97400 <humblot.david1@gmail.com>
2020-12-01 11:34:06 +0400

    card css

--- src/includes/EnchereList.php
+++ src/includes/EnchereList.php
@@ -26,29 +26,34 @@
 </form>
     </div>
   </div>
-  <div class="card">
-    <img src="ressources/img/BUDHA.jpg" class="card-img-top" alt="...">
+  <div class="card ">
+    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
     <div class="card-body">
-    <h5 class="card-title">NOM PRODUIT</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <h4 class="card-title">NOM PRODUIT</h4>
+
+      <h5 class = "card-title">Prix :</h5>
+
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <!--Ajout du bouton enchérire-->
       <form>
       <button type="button" class="btn btn-success">Enchérir</button>
 </form>
     </div>
   </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+  <div class="card ">
+    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
     <div class="card-body">
-    <h5 class="card-title">NOM PRODUIT</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h4 class="card-title">NOM PRODUIT</h4>
+
+      <h5 class = "card-title">Prix :</h5>
+
+      <p class="card-text">This is a longer card with czsxcszdvcscscsdcsdcfzdcvzdvedvzdvedvcdjzhqbfjzheibfjhgebjhvfbcjzhedgfbjszdvdvzevzefzevzfesupporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <!--Ajout du bouton enchérire-->
       <form>
       <button type="button" class="btn btn-success">Enchérir</button>
 </form>
     </div>
   </div>
-</div>
 
     <!--Seconde rangé de trois enchères-->
     
