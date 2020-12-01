c078f7efba41adcdc0e6ef557476ddd138c9d8bf
Dada97400 <humblot.david1@gmail.com>
2020-12-01 10:37:47 +0400

    choix cdu composant card et placement correcte

---
+++ src/includes/EnchereList.php
@@ -0,0 +1,82 @@
+<!--Page ou on aura le visuelle des encheres ajouter apres utilisation du formumaire d'ajout-->
+
+<div class="container_fluid pb-5 mb-5">
+  <!--AJOUT DU TIRE-->
+  <div class="container d-flex justify-content-center">
+    <h2>Enchère en cours</h2>
+  </div>
+
+  <!--AJOUT VISUEL DES ENCHERES (BRUT)-->
+
+  <div class="container ">
+
+    <!--Première ranger de 3 carte-->
+    <div class="card-deck mt-5">
+  <div class="card ">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+    <!--Seconde rangé de trois enchères-->
+    
+    <div class="card-deck mt-5">
+  <div class="card ">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+  </div>
+
+
+
+
+
+</div>
