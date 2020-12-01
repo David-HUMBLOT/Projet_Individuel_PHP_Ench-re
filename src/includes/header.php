1dccdb8b4291a92a3db2bf76ef34fbc2f7decdd8
Dada97400 <humblot.david1@gmail.com>
2020-12-01 09:36:33 +0400

    placement et animatios pour les texts de la nav

--- src/includes/header.php
+++ src/includes/header.php
@@ -3,11 +3,12 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light sticky-top mb-5" id="mainNav">
+
         <div class="container-fluid">
           <!--Ajout d'un classe pour les boutons nav-->
             <a class="navbar-brand js-scroll-trigger text-white font-weight-bold" href="#page-top" id="title-header">Ventes aux enchères</a>
 
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            <button class="navbar-toggler navbar-toggler-right " type="button" data-toggle="collapse"
             data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
             aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
