7a9bd18ee9a8b2b9e6ed3b51dda67a70b8b8604f
Dada97400 <humblot.david1@gmail.com>
2020-12-01 11:31:20 +0400

    dimensionnement des card

--- src/includes/EnchereList.php
+++ src/includes/EnchereList.php
@@ -13,27 +13,39 @@
     <!--Première ranger de 3 carte-->
     <div class="card-deck mt-5">
   <div class="card ">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h4 class="card-title">NOM PRODUIT</h4>
+
+      <h5 class = "card-title">Prix :</h5>
+
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <!--Ajout du bouton enchérire-->
+      <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="ressources/img/BUDHA.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">NOM PRODUIT</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <!--Ajout du bouton enchérire-->
+      <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
     </div>
   </div>
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">NOM PRODUIT</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <!--Ajout du bouton enchérire-->
+      <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
     </div>
   </div>
 </div>
@@ -44,25 +56,34 @@
   <div class="card ">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">NOM PRODUIT</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+       <!--Ajout du bouton enchérire-->
+       <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
     </div>
   </div>
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+    <div class="card-body"><h5 class="card-title">NOM PRODUIT</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+       <!--Ajout du bouton enchérire-->
+       <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
     </div>
   </div>
   <div class="card">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">NOM PRODUIT</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <!--Ajout du bouton enchérire-->
+      <form>
+      <button type="button" class="btn btn-success">Enchérir</button>
+</form>
+      
     </div>
   </div>
 </div>
