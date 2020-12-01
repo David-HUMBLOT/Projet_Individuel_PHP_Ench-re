d9329f2a84679f4261694c69747def9499d4cf02
Dada97400 <humblot.david1@gmail.com>
2020-12-01 09:20:19 +0400

    couleur nav

--- src/includes/header.php
+++ src/includes/header.php
@@ -2,19 +2,26 @@
 
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-success sticky-top mb-5" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top mb-5" id="mainNav">
         <div class="container-fluid">
+          <!--Ajout d'un classe pour les boutons nav-->
             <a class="navbar-brand js-scroll-trigger text-white font-weight-bold" href="#page-top" id="title-header">Ventes aux enchères</a>
+
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
             data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
             aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto my-2 my-lg-0">
                     <li class="nav-item">
+
                         <a class="nav-link js-scroll-trigger text-white effect-underline font-weight-bold" id="listeenchere" href="index.php">Liste des enchères</a>
+
                     </li>
                     <li class="nav-item">
+
                         <a class="nav-link js-scroll-trigger text-white font-weight-bold" href="ajoutEnchere.php" id="encherenav">Ajouter une enchère</a>
+
                     </li>
                 </ul>
             </div>
