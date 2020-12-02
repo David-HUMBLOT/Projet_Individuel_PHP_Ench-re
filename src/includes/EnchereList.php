0e942261be1fb5daaa3f8dd9aa71a91573dfc6d7
Dada97400 <humblot.david1@gmail.com>
2020-12-02 08:31:05 +0400

    test

--- src/includes/EnchereList.php
+++ src/includes/EnchereList.php
@@ -12,98 +12,68 @@
 
     <!--Première ranger de 3 carte-->
     <div class="card-deck mt-5">
-  <div class="card ">
-    <div class = "timer"><p>TIMER</p></div>
-    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h4 class="card-title">NOM PRODUIT</h4>
+    <div class="card ">
+        <div class="timer">
+          <p>TIMER</p>
+        </div>
+        <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title">NOM PRODUIT</h4>
+          <h5 class="card-title">Prix :</h5>
+          <div class = "card-text">
+          <p >Prix/clic: </p>
+          <p>Prix/enchère: </p>
+          </div>
+        
+          <!--Ajout du bouton enchérire-->
+          <form>
+            <button type="button" class="btn btn-success">Enchérir</button>
+          </form>
+        </div>
+      </div>
 
-      <h5 class = "card-title">Prix :</h5>
 
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <!--Ajout du bouton enchérire-->
-      <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
+
+      <div class="card ">
+        <div class="timer">
+          <p>TIMER</p>
+        </div>
+        <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title">NOM PRODUIT</h4>
+          <h5 class="card-title">Prix :</h5>
+          <div class = "card-text">
+          <p >Prix/clic: </p>
+          <p>Prix/enchère: </p>
+          </div>
+        
+          <!--Ajout du bouton enchérire-->
+          <form>
+            <button type="button" class="btn btn-success">Enchérir</button>
+          </form>
+        </div>
+      </div>
+
+
+      <div class="card ">
+        <div class="timer">
+          <p>TIMER</p>
+        </div>
+        <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title">NOM PRODUIT</h4>
+          <h5 class="card-title">Prix :</h5>
+          <div class = "card-text">
+          <p >Prix/clic: </p>
+          <p>Prix/enchère: </p>
+          </div>
+        
+          <!--Ajout du bouton enchérire-->
+          <form>
+            <button type="button" class="btn btn-success">Enchérir</button>
+          </form>
+        </div>
+      </div>
+
     </div>
   </div>
-  <div class="card ">
-    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h4 class="card-title">NOM PRODUIT</h4>
-
-      <h5 class = "card-title">Prix :</h5>
-
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <!--Ajout du bouton enchérire-->
-      <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
-    </div>
-  </div>
-  <div class="card ">
-    <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h4 class="card-title">NOM PRODUIT</h4>
-
-      <h5 class = "card-title">Prix :</h5>
-
-      <p class="card-text">This is a longer card with czsxcszdvcscscsdcsdcfzdcvzdvedvzdvedvcdjzhqbfjzheibfjhgebjhvfbcjzhedgfbjszdvdvzevzefzevzfesupporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <!--Ajout du bouton enchérire-->
-      <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
-    </div>
-  </div>
-
-    <!--Seconde rangé de trois enchères-->
-    
-    <div class="card-deck mt-5">
-  <div class="card ">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">NOM PRODUIT</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       <!--Ajout du bouton enchérire-->
-       <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body"><h5 class="card-title">NOM PRODUIT</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-       <!--Ajout du bouton enchérire-->
-       <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">NOM PRODUIT</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <!--Ajout du bouton enchérire-->
-      <form>
-      <button type="button" class="btn btn-success">Enchérir</button>
-</form>
-      
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-  </div>
-
-
-
-
-
-</div>
