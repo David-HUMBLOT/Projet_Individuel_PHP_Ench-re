b198e8cba2c5355ac71e246b630070a565472c45
Dada97400 <humblot.david1@gmail.com>
2020-12-02 11:08:53 +0400

    vvérification du stockage des donnée dans un tableaux array

--- src/includes/traitement.php
+++ src/includes/traitement.php
@@ -10,40 +10,37 @@
 
 $nomProduit = $_POST["nomProduit"];
 $prixInitial = $_POST["prixInitial"];
-
 $PrixClic = $_POST["PrixClic"];
-
 $upClic = $_POST["upClic"];
 $time = $_POST["time"];
 $upTime = $_POST["upTime"];
 $choixImage = $_POST["choixImage"];
 
+/**Stockage sous forme de tableau et dans une seul variable les infos saisie depuis le formumaire apres un envoie valide*/
+$data_stock_post = array ($nomProduit,$prixInitial,$PrixClic,$upClic,$time,$upTime,$choixImage);
 
-echo $nomProduit;
-echo $prixInitial;
 
-echo $PrixClic;
-
-echo $upClic;
-echo $time;
-echo $upTime;
-echo $choixImage;
+/**pour la vérification sous forme de tableaux des éléments posté */
+var_dump ("data_stock_post");
+print_r ($data_stock_post);
 
 
 ?>
 
-<!--
 
 
-$prixInitial = 
-$prixClic = 
-$upClic = 
-$time = 
-$upTime = 
-$choixImage = 
 
-//post_data_recup stocke les données sous forme d'un tableau que l'on convertira au format JSON
-$post_data_recup = array
 
+
+
+<!-- Pour la vérification de récupération des input :
+
+echo $nomProduit;
+echo $prixInitial;
+echo $PrixClic;
+echo $upClic;
+echo $time;
+echo $upTime;
+echo $choixImage;
 
 -->
