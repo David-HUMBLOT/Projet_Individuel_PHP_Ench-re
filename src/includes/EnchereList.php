9bf2bca3893f8fc3ed2546998351b38cd1f7f254
Dada97400 <humblot.david1@gmail.com>
2020-12-03 18:46:54 +0400

    soucis card résolu

--- src/includes/EnchereList.php
+++ src/includes/EnchereList.php
@@ -5,54 +5,13 @@
 
 
 
-<div class="container-fluid pb-5 mb-5 ">
-  <div class="container col-6">
+<!--Balise qui conditionne le comportement des card qui sont généré par ajout du formulaire-->
+  <div class="container-fluid row">
   
   <?php include 'src/includes/AjoutEnchereList.php'?>
-  <div class="card-deck mt-5">
-      <div class="card ">
-        <div class="timer">
-          <p>TIMER</p>
-        </div>
-        <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">NOM PRODUIT</h4>
-          <h5 class="card-title">Prix :</h5>
-          <div class="card-text">
-            <p>Prix/clic: </p>
-            <p>Prix/enchère: </p>
-          </div>
-          <form>
-            <button type="button" class="btn btn-success">Enchérir</button>
-          </form>
-        </div>
-      </div>
-</div>
-<div class="card-deck mt-5">
-      <div class="card ">
-        <div class="timer">
-          <p>TIMER</p>
-        </div>
-        <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">NOM PRODUIT</h4>
-          <h5 class="card-title">Prix :</h5>
-          <div class="card-text">
-            <p>Prix/clic: </p>
-            <p>Prix/enchère: </p>
-          </div>
-          <form>
-            <button type="button" class="btn btn-success">Enchérir</button>
-          </form>
-        </div>
-      </div>
-</div>
-
-
-
 
 
     </div>
-  </div>
+  
 </div>
 
