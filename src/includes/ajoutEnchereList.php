cc1b3a40352fc275331824a7d70f4e263a11e2ed
Dada97400 <humblot.david1@gmail.com>
2020-12-04 09:26:08 +0400

    minification

--- src/includes/ajoutEnchereList.php
+++ src/includes/ajoutEnchereList.php
@@ -1,7 +1,7 @@
 <?php
 include 'traitement.php' ?>
 <!--GENERATEUR DES CARD AVEC LES INFOS DES POST-->
-<div class="container d-flex justify-content-center">
+<div class="container-fluid d-flex justify-content-center">
   <h2>Enchère en cours</h2>
   
 </div>
@@ -12,25 +12,27 @@ include 'traitement.php' ?>
 $data_stock_post_string = file_get_contents('data.json');
 $data_stock_post_array = json_decode($data_stock_post_string, true);
 ?>
-<div class="container-fluid row">
+<div class="container-fluid row d-flex justify-content-around">
   <!----------------------------------------------------------------------------------------->
   <!--Il faut que pour chaque tableaux present dans le json, on genere le nombre de card correspondante-->
   <?php
-  foreach ($data_stock_post_array as $value) {
-    echo 'TEST ok ';
+
+  foreach ($data_stock_post_array as $key => $items) {
+   //echo $items['nomProduit'];
   ?>
+  <!--A chaque nouvelle carte récuperer les donnée de json pour affecté aux bon emplcement sur le carte-->
     <div class="card-deck mt-5">
       <div class="card ">
         <div class="timer">
-          <p>TIMER</p>
+          <p><?=$items['time'];?></p>
         </div>
         <img src="ressources/img/no_image.png" class="card-img-top" alt="...">
         <div class="card-body">
-          <h4 class="card-title">NOM PRODUIT</h4>
-          <h5 class="card-title">Prix :</h5>
+          <h5 class="card-title"> <?=$items['nomProduit'];?></h>
+          <h6 class="card-title pt-3">Prix:<?=$items['prixInitial'];?> €</h6>
           <div class="card-text">
-            <p>Prix/clic: </p>
-            <p>Prix/enchère: </p>
+            <p>Prix/clic: <?=$items['PrixClic'];?>€</p>
+            <p>Prix/enchère:€</p>
           </div>
           <form>
             <button type="button" class="btn btn-success">Enchérir</button>
--- src/includes/traitement.php
+++ src/includes/traitement.php
@@ -1,29 +1,18 @@
 
 
 <?php 
-
 $json = 'data.json';
-
-/******************************************************************************************************* */
-/**CAS 1 SI UN AJOUT EST EFFECTUER */
+/**AJOUT EST EFFECTUER */
 if (isset($_POST["ajoutEnchere"]))
-
-
-{
-    
-    $data_stock_post =  
-    
-    array(
+{  $data_stock_post =  array(
 'nomProduit' => $_POST['nomProduit'],
 'prixInitial' => $_POST['prixInitial'],
 'PrixClic' => $_POST['PrixClic'],
 'upClic' => $_POST['upClic'],
 'time' => $_POST['time'],
 'upTime' => $_POST['upTime'],
-'choixImage' => $_POST['choixImage'], 
+'choixImage' => $_POST['choixImage'],  );
  /**servira à recuperer une enchere particuliere */
-
-    );
 /**AJPUT D UN NOUVELE ELEMENT A JSON SANS ECRASER LA DONN2 EXISTANT */
 /***ouvrir (decode) le fichier dans une var $tab*/
 $data_stock_post_string = file_get_contents('data.json');
@@ -33,10 +22,8 @@ $data_stock_post_array = json_decode($data_stock_post_string, true);
 /**enregistret cette modif dans data.json */
 array_push ($data_stock_post_array,$_POST);
 file_put_contents("data.json", json_encode ($data_stock_post_array));
-
 };
-/**UNE FOIS LES DONN2E ENREGISTRER IN VEUT QUE A CHAQUE SUBMIT ON GENERER L AFFICHAGE DE LA CAARD AVEC LES DONN2ES POST EN QUESTION Recuperer sur le json à son bon emplacemnt, ^revoir des id pour chaque element*/
-
+/**UNE FOIS LES DONN2E ENREGISTRER IN VEUT QUE A CHAQUE SUBMIT ON GENERER L AFFICHAGE DE LA CARD AVEC LES DONN2ES POST EN QUESTION Recuperer sur le json à son bon emplacemnt, ^revoir des id pour chaque element*/
 ?>
 
 
