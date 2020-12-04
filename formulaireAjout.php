2fed99e41c2864cdd9140c193b7aea862c871547
Dada97400 <humblot.david1@gmail.com>
2020-12-04 09:31:36 +0400

    minification

--- formulaireAjout.php
+++ formulaireAjout.php
@@ -3,8 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Enchère_Plateforme</title>
   <!-- BOOTSTRAP -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <!-- CSS -->
   <link rel="stylesheet" href="src/css/VisuelEnchereList.css">
   <link rel="stylesheet" href="src/css/navbar.css">
@@ -12,38 +11,28 @@
 
 <body>
   <!--AJOUT INCLUDE HEADER-->
-  <?php include 'src/includes/header.php'?>
   <!--Ajout page des fonctions-->
-<?php include 'src/includes/functions.php' ?>
-
   <!--Ajout page traitement.php-->
+  <?php include 'src/includes/header.php' ?>
+  <?php include 'src/includes/functions.php' ?>
   <?php include 'src/includes/traitement.php' ?>
-
-    
-
   <!--Formulaire pour la saisie des données pour la déclaration d'une nouvelle enchere-->
-
-  <div class="container-fluid d-flex justify-content-center">
-    <h2>Ajouter une enchére :</h2>
-  </div>
-
+  <div class="container-fluid d-flex justify-content-center mb-5"><h2>Ajouter une enchére :</h2></div>
   <!--ici on placera un message pour confirmer que l'article à bien été ajouté-->
-<?php if (isset($_POST["ajoutEnchere"])){ ?>
-<div class="container d-flex justify-content-center"><p>Votre enchère à bien été ajouté.</p></div>
-<?php }; ?>
+  <?php if (isset($_POST["ajoutEnchere"])) { ?>
+    <div class="container d-flex justify-content-center">
+      <p>Votre enchère à bien été ajouté.</p></div>
+  <?php }; ?>
 
   <div class="container">
     <div class="row">
       <div class="col">
-       Tu est à gauche
+    
       </div>
       <div class="col-6">
-
-
-
-<!--Creéation formulaire et champs input -->
+        <!--Creéation formulaire et champs input -->
         <div>
-          <form  method="POST" action="">
+          <form method="POST" action="">
             <table>
               <tr>
                 <td><label>NomProduit</label></td>
@@ -72,7 +61,7 @@
               <!--Choisire une image de type file coté admin-->
               <tr>
                 <td><label>Choix image</label></td>
-                <td><input type="file" value="" name="choixImage" id="choixImage" ></td>
+                <td><input type="file" value="" name="choixImage" id="choixImage"></td>
               </tr>
               <!--BOUTON DE VALIDATION A PARAMETRER-->
               <tr>
@@ -81,21 +70,20 @@
               </tr>
             </table>
           </form>
-
         </div>
       </div>
 
-    <div class="col">
-       Tu est à droite
+      <div class="col">
+      
       </div>
     </div>
   </div>
 
-    
 
 
 
-  
+
+
 
 
 
--- src/includes/traitement.php
+++ src/includes/traitement.php
@@ -12,18 +12,14 @@ if (isset($_POST["ajoutEnchere"]))
 'time' => $_POST['time'],
 'upTime' => $_POST['upTime'],
 'choixImage' => $_POST['choixImage'],  );
- /**servira à recuperer une enchere particuliere */
-/**AJPUT D UN NOUVELE ELEMENT A JSON SANS ECRASER LA DONN2 EXISTANT */
-/***ouvrir (decode) le fichier dans une var $tab*/
+ /**servira à recuperer une enchere particuliere _ AJoUT D UN NOUVELE ELEMENT A JSON SANS ECRASER LA DONN2 EXISTANT _ ouvrir (decode) le fichier dans une var $tab*/
 $data_stock_post_string = file_get_contents('data.json');
 $data_stock_post_array = json_decode($data_stock_post_string, true);
-/**mettre $data_stock_post dans la var $tab */
-/*$data_stock_post_array =  $data_stock_post_string.json_encode($data_stock_post);;
-/**enregistret cette modif dans data.json */
+/**mettre $data_stock_post dans la var $tab __ $data_stock_post_array =  $data_stock_post_string.json_encode($data_stock_post); _ enregistret cette modif dans data.json 
+UNE FOIS LES DONN2E ENREGISTRER IN VEUT QUE A CHAQUE SUBMIT ON GENERER L AFFICHAGE DE LA CARD AVEC LES DONN2ES POST EN QUESTION Recuperer sur le json à son bon emplacemnt, ^revoir des id pour chaque element*/
 array_push ($data_stock_post_array,$_POST);
 file_put_contents("data.json", json_encode ($data_stock_post_array));
 };
-/**UNE FOIS LES DONN2E ENREGISTRER IN VEUT QUE A CHAQUE SUBMIT ON GENERER L AFFICHAGE DE LA CARD AVEC LES DONN2ES POST EN QUESTION Recuperer sur le json à son bon emplacemnt, ^revoir des id pour chaque element*/
 ?>
 
 
